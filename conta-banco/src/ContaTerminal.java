a29ecb90bd07d48e7a7392f3afce64d6abf5ad7b
LuizEduardo-Teixeira <luizteixeiraeduardo@gmail.com>
2024-05-14 20:27:36 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the terminal
+        
         Scanner scanner = new Scanner(System.in);
 
-        // Declare variables with meaningful names and follow Java naming conventions
         int numero;
         String agencia;
         String nomeCliente;
         double saldo;
 
-        try (// Prompt the user to enter the required information
+        try (
         var out = System.out) {
             out.print("Por favor, digite o número da conta: ");
             numero = scanner.nextInt();
@@ -21,7 +20,7 @@ public class ContaTerminal {
 
             out.print("Por favor, digite o nome do Cliente: ");
             String nomeClienteInput = scanner.next();
-            // Keep prompting the user until a valid name is entered
+            
             while (nomeClienteInput.matches("\\d+")) {
                 out.print("Por favor, digite um nome válido: ");
                 nomeClienteInput = scanner.next();
@@ -29,20 +28,17 @@ public class ContaTerminal {
             nomeCliente = nomeClienteInput;
 
             out.print("Por favor, digite o saldo inicial: ");
-            // Keep prompting the user until a valid double value is entered
+            
             while (!scanner.hasNextDouble()) {
                 out.print("Por favor, digite um valor numérico válido: ");
-                scanner.next(); // Discard the invalid input
+                scanner.next(); 
             }
             saldo = scanner.nextDouble();
 
-            // Close the Scanner object to prevent resource leaks
             scanner.close();
 
-            // Use string concatenation to build the output message
             String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
 
-            // Print the output message
             out.println(mensagem);
         }
     }
